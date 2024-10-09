3585a5e4719a45af1cfbca132b34b48d5bee5b2c
Hritikupadhyay <102146043+Hritikupadhyay@users.noreply.github.com>
2024-10-09 14:06:52 +0530

    Update Treasure_Islands.java

--- Treasure_Islands.java
+++ Treasure_Islands.java
@@ -8,36 +8,36 @@ public class TreasureIsland {
 		
 		Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Welcome to Treasure Island, Your mission is to find treasure.");
+        System.out.println("Welcome to Treasure Island, Your mission is to find treasure");
 
         
-        System.out.print("Do you want to go 'left' or 'right'? ");
+        System.out.print("Do you want to go left or right? ");
         String direction = scanner.nextLine().toLowerCase();
 
         if (direction.equals("left")) {
             
-            System.out.print("Do you want to 'swim' or 'wait'? ");
+            System.out.print("Do you want to swim or wait? ");
             String action = scanner.nextLine().toLowerCase();
 
             if (action.equals("wait")) {
                 
-                System.out.print("Which door? 'red', 'blue', or 'yellow'? ");
+                System.out.print("Which door, red, blue, or yellow? ");
                 String door = scanner.nextLine().toLowerCase();
 
                 if (door.equals("yellow")) {
                     System.out.println("You Win!");
                 } else if (door.equals("red")) {
-                    System.out.println("Burned by fire. Game Over.");
+                    System.out.println("Burned by fire, Game Over");
                 } else if (door.equals("blue")) {
-                    System.out.println("Eaten by beasts. Game Over.");
+                    System.out.println("Eaten by beasts, Game Over");
                 } else {
-                    System.out.println("Game Over.");
+                    System.out.println("Game Over");
                 }
             } else {
-                System.out.println("Attacked by trout. Game Over.");
+                System.out.println("Attacked by trout, Game Over");
             }
         } else {
-            System.out.println("Fall into a hole. Game Over.");
+            System.out.println("Fall into a hole, Game Over");
         }
 
         
